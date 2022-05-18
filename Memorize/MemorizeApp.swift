9d5461cc0af2cb21e96a20b35c48973fdb0b9da9
sunset wan <sunsetwan@gmail.com>
2022-05-18 14:45:16 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sunsetwan on 2022/5/18.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
