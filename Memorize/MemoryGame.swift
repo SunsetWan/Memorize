278abd325cabb750173011b0620c96b8bc3a58b7
sunset wan <sunsetwan@gmail.com>
2022-05-25 17:34:59 +0800

    feat: add card "Bonus Time" mechanism

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -19,6 +19,64 @@ struct MemoryGame<CardContent> where CardContent: Hashable {
         var isMatched: Bool = false
         let content: CardContent
         let id: Int
+        
+        /// MARK: - Bonus Time
+        
+        /// This could give matching bonus points
+        /// if the user matches the card
+        /// before a certain amount of time passes during which the card is face up
+        
+        /// Can be zero which means "no nonus available" for this card.
+        var bonusTimeLimit: TimeInterval = 6
+        
+        /// How long this card has ever been face up
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        
+        /// The last time this card was turned face up (and is still face up)
+        var lastFaceUpDate: Date?
+        
+        /// The accumulated time this card has been face up in the past
+        /// (i.e. not including the current time it's been face up if it's currently so)
+        var pastFaceUpTime: TimeInterval = 0
+        
+        /// How much time left before the bonus opportunity runs out
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        
+        /// Percentage of the bonus time remaining
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining / bonusTimeLimit : 0
+        }
+        
+        /// Whether the card was matched during the bonus time period
+        var hasEarnedBonus: Bool {
+            isMatched && bonusRemaining > 0
+        }
+        
+        /// Whether we are currently face up, unmatched and have not yet used up the bonus window
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        /// Called when the card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        
+        /// Called when the card goes back face down (or gets matched)
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
     }
     
     private(set) var cards: [Card]
@@ -62,7 +120,6 @@ struct MemoryGame<CardContent> where CardContent: Hashable {
         cards.shuffle()
     }
 }
-
 
 extension Array {
     var oneAndOnly: Element? {
